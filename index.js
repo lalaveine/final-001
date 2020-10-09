9f181804d0c77a185941b063e379e2927ca73d1d
Evgenii Matveev <mfb.eugene@gmail.com>
2020-10-09 19:55:53 +0300

    test 1

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-require('http')
-.Server((req, res) => {
-  
-  const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-};
-  res.writeHead(200, CORS);
-  if (req.url === '/v8') return res.end(process.versions.v8);
-  if (req.url === '/package.json') return require('fs').createReadStream('./package.json').pipe(res);
-  
-  res.end('student2020heroku');
-})
-.listen(process.env.PORT);
+require("http")
+  .Server((req, res) => {
+    const CORS = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,DELETE",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Access-Control-Allow-Headers",
+    };
+    res.writeHead(200, CORS);
+    if (req.url === "/v8") return res.end(process.versions.v8);
+    if (req.url === "/package.json")
+      return require("fs").createReadStream("./package.json").pipe(res);
+    if (req.url === "/day") return new Date().getDate();
+    if (req.url.includes("/mirror?x="))
+      return req.url.replace("/mirror?x=", "");
+
+    res.end("evgeniimatveev");
+  })
+  .listen(process.env.PORT);
