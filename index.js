77278e594fc29cbad653b0e54fd01387e326dab2
Evgenii Matveev <mfb.eugene@gmail.com>
2020-12-06 14:49:20 +0300

    task 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("http")
   .Server((req, res) => {
+    const NAME = "itmo287659"
     const CORS = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET,POST,DELETE",
@@ -7,14 +8,18 @@ require("http")
         "Content-Type, Access-Control-Allow-Headers",
     };
     res.writeHead(200, CORS);
-    if (req.url === "/v8") return res.end(process.versions.v8);
-    if (req.url === "/node") return res.end(process.versions.node);
-    if (req.url === "/package.json")
-      return require("fs").createReadStream("./package.json").pipe(res);
-    if (req.url === "/day") return res.end(`${new Date().getDate()}`);
-    if (req.url.includes("/mirror?x="))
-      return res.end(req.url.replace("/mirror?x=", ""));
+    const HEADERS = {
+      "X-Author": NAME
+    }
+    res.writeHead(200, HEADERS);
+    // if (req.url === "/v8") return res.end(process.versions.v8);
+    // if (req.url === "/node") return res.end(process.versions.node);
+    // if (req.url === "/package.json")
+    //   return require("fs").createReadStream("./package.json").pipe(res);
+    // if (req.url === "/day") return res.end(`${new Date().getDate()}`);
+    // if (req.url.includes("/mirror?x="))
+    //   return res.end(req.url.replace("/mirror?x=", ""));
 
-    res.end("evgeniimatveev");
+    res.end(NAME);
   })
   .listen(process.env.PORT);
