7704d9d939f8055048769436283a49fe1ec2917f
Evgenii Matveev <mfb.eugene@gmail.com>
2021-01-03 20:37:31 +0300

    test4

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ export default (
       createReadStream(import.meta.url.substring(7)).pipe(res);
     })
     .get("/test/", (req, res) => {
-      console.log("test" + req.url);
+      console.log("test" + req.url.split("%2F"));
     });
 
   app.post("/insert/", async (req, res) => {
