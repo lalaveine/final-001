84c49039086d365bce9492b0405902354b478bd5
Evgenii Matveev <mfb.eugene@gmail.com>
2021-01-03 11:58:14 +0300

    les5

---
+++ app.js
@@ -0,0 +1,56 @@
+import express from "express";
+import fs from "fs";
+import crypto from "crypto";
+import http from "http";
+
+const CORS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers":
+    "x-test,Content-Type,Accept, Access-Control-Allow-Headers",
+};
+
+function myFunc(express, bodyParser, fs, crypto, http) {
+  const app = express();
+
+  app.port = process.env.PORT || 4321;
+
+  app
+    .use((req, res, next) => {
+      res.set(CORS);
+      next();
+    })
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get("/sha1/:input", (req, res) => {
+      let hash = crypto.createHash("sha1");
+      hash.update(req.params.input);
+      res.send(hash.digest("hex"));
+    })
+
+    .get("/login/", (req, res) => res.send("itmo287659"))
+    .get("/code/", (req, res) => {
+      let filename = import.meta.url.substring(8);
+      fs.createReadStream(filename).pipe(res);
+    });
+
+  app.all("/req/", (req, res) => {
+    let url = req.query.addr;
+    http.get(url, (response) => {
+      let data = "";
+      response.on("data", (chunk) => (data += chunk));
+      response.on("end", () => {
+        res
+          .set({
+            "Content-Type": "text/plain; charset=utf-8",
+          })
+          .end(data);
+      });
+    });
+  });
+  app.all("*", (req, res) => {
+    res.send("itmo287659");
+  });
+  return app;
+}
+
+export default myFunc;
--- routes/api2/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (x) => {
-  const router = x.Router();
-  router.all('/', (r) => {
-    router.all(/hello/, (r) => r.res.send('hello'));
-    r.res.send('Not working yet');
-  });
-
-  return router;
-};
